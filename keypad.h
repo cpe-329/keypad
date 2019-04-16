4abfb2338fca331084b1eaf2db1e59dc513d46e0
Spencer Shaw <sfshaw@calpoly.edu>
2019-04-16 10:37:18 -0700

    add signatures for keypad internal function

--- keypad.h
+++ keypad.h
@@ -1,4 +1,4 @@
-/**
+/*
  * keypad.h
  * 
  * Danica Fujiwara & Spencer Shaw
@@ -14,7 +14,33 @@
 
 #include "my_msp.h"
 
+/* Keypad Pins: NC  A     B     C     D     E     F     G    NC
+ * MSP Pins:       Px.y  Px.y  Px.y  Px.y  Px.y  Px.y  Px.y
+ * Pin Name:       COL1  ROW0  COL0  ROW3  COL2  ROW2  ROW1
+ *     
+ */
+
+
+#define ROW_MASK (P | P | P | P)
+// Enumeration of rows
+#define ROW_0_SELECT (BIT0)
+#define ROW_1_SELECT (BIT1)
+#define ROW_2_SELECT (2)
+#define ROW_3_SELECT (3)
+
+
 void key_init(void);
 uint8_t keypad_getkey(void);
+
+inline static void row_0_select();
+inline static void row_1_select();
+inline static void row_2_select();
+inline static void row_2_select();
+
+inline static u8 col_0_high();
+inline static u8 col_1_high();
+inline static u8 col_2_high();
+
+inline u8 translate_keypad_rgb();
 
 #endif
