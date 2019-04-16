0c5145cf2bb698c9b8a81c41ae905ace43428445
Spencer Shaw <sfshaw@calpoly.edu>
2019-04-16 12:54:22 -0700

    Fill out keypad driving code

--- keypad.c
+++ keypad.c
@@ -5,63 +5,103 @@
  * 
  * CPE 329-17/18 Spring 2019
  */
+#include "msp.h"
+
+#include "my_msp.h"
 
 #include "keypad.h"
 #include "delay.h"
-#include "msp.h"
+#include "led.h"
 
-void key_init(void){
-    uint8_t col, row, key;
-    P3->DIR = R1|R2|R3|R0;
-    P3->REN = C1|C2|C3;
-    P3->OUT &= ~(C1|C2|C3);
+inline void keypad_init(void){
+    P2->SEL0 &= ~COL_MASK;
+    P2->SEL1 &= ~COL_MASK;
+    P2->DIR |= COL_MASK;
+    P2->REN |= COL_MASK;
+    P2->OUT &= ~COL_MASK;
+
+    P5->SEL0 &= ~ROW_MASK;
+    P5->SEL1 &= ~ROW_MASK;
+    P5->DIR &= ~ROW_MASK;   
 }
 
 uint8_t keypad_getkey(void){
-    uint8_t col, row;
+    row_all_select();
+    delay_ms_auto(1);
+    if ((~col_0_read()) & 
+        (~col_1_read()) &
+        (~col_2_read()))
+        return 12;
 
-    row_0_select()// put row high
-
-    delay_ms(25); //delay for 25 cycles
-
-    //col = P3->IN &(C1|C2|C3); // masks columns
-    if(col_0_high())
+    row_0_select();  // put row high
+    delay_ms_auto(1); //delay for 25 cycles
+    if(col_0_read())
        return 1;
-    else if (col_1_high())
+    else if (col_1_read())
        return 2;
-    else if (col_2_high())
+    else if (col_2_read())
        return 3;
 
-    row_1_select()// put row high
-
-    delay_ms(25); //delay for 25 cycles
-
-    if(col_0_high())
+    row_1_select();  // put row high
+    delay_ms_auto(1); //delay for 25 cycles
+    if(col_0_read())
        return 4;
-    else if (col_1_high())
+    else if (col_1_read())
        return 5;
-    else if (col_2_high())
+    else if (col_2_read())
        return 6;
 
-    row_2_select()// put row high
-
-    delay_ms(25); //delay for 25 cycles
-
-    if(col_0_high())
+    row_2_select();  // put row high
+    delay_ms_auto(1); //delay for 25 cycles
+    if(col_0_read())
         return 7;
-    else if (col_1_high())
+    else if (col_1_read())
         return 8;
-    else if (col_2_high())
+    else if (col_2_read())
         return 9;
 
-    row_3_select()// put row high
-
-    delay_ms(25); //delay for 25 cycles
-
-    if(col_0_high())
+    row_3_select();  // put row high
+    delay_ms_auto(1);  //delay for 25 cycles
+    if(col_0_read())
         return 10;
-    else if (col_1_high())
+    else if (col_1_read())
         return 0;
-    else if (col_2_high())
+    else if (col_2_read())
         return 11;
+    return 12;
+}
+
+inline static void row_all_select(){
+    P2->OUT |= COL_MASK;
+}
+
+inline static void row_0_select(){
+    P2->OUT |= ROW0;
+    P2->OUT &= ~(ROW1 | ROW2);
+}
+
+inline static void row_1_select(){
+    P2->OUT |= ROW1;
+    P2->OUT &= ~(ROW0 | ROW2);
+}
+
+inline static void row_2_select(){
+    P2->OUT |= ROW2;
+    P2->OUT &= ~(ROW0 | ROW1);
+}
+
+inline static u8 col_all_read(){
+    return P5->IN & COL_MASK;
+}
+
+inline static u8 col_0_read(){
+    return P5->IN & COL0;
+}
+
+inline static u8 col_1_read(){
+    return P5->IN & COL1;
+}
+
+inline static u8 col_2_read(){
+    return P5->IN & COL2;
 }
--- keypad.h
+++ keypad.h
@@ -15,32 +15,37 @@
 #include "my_msp.h"
 
 /* Keypad Pins: NC  A     B     C     D     E     F     G    NC
- * MSP Pins:       Px.y  Px.y  Px.y  Px.y  Px.y  Px.y  Px.y
+ * MSP Pins:       P2.6  P5.0  P2.5  P5.7  P2.7  P5.6  P5.1
  * Pin Name:       COL1  ROW0  COL0  ROW3  COL2  ROW2  ROW1
  *     
  */
 
+#define COL0 BIT5
+#define COL1 BIT6
+#define COL2 BIT7
 
-#define ROW_MASK (P | P | P | P)
-// Enumeration of rows
-#define ROW_0_SELECT (BIT0)
-#define ROW_1_SELECT (BIT1)
-#define ROW_2_SELECT (2)
-#define ROW_3_SELECT (3)
+#define ROW0 BIT0
+#define ROW1 BIT1
+#define ROW2 BIT2
+#define ROW3 BIT3
+
+#define COL_MASK (COL2 | COL1 | COL0)
+#define ROW_MASK (ROW3 | ROW2 | ROW1 | ROW0)
 
 
-void key_init(void);
-uint8_t keypad_getkey(void);
+inline void keypad_init(void);
+u8 keypad_getkey(void);
 
+inline static void row_all_select();
 inline static void row_0_select();
 inline static void row_1_select();
 inline static void row_2_select();
 inline static void row_2_select();
 
-inline static u8 col_0_high();
-inline static u8 col_1_high();
-inline static u8 col_2_high();
+inline static u8 col_all_read();
+inline static u8 col_0_read();
+inline static u8 col_1_read();
+inline static u8 col_2_read();
 
-inline u8 translate_keypad_rgb();
 
 #endif
